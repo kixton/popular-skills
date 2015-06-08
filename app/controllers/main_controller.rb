af8e9099180d4a6c8054b6a97c19fcb0eb3252f0
Kim Pham <ms.kimpham@gmail.com>
2015-06-08 14:08:22 -0700

    Add methods to query AngelList api and calculate stats

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,34 +2,68 @@ require 'date'
 
 class MainController < ApplicationController
   include HTTParty
-  
+  attr_accessor :stats
+
   def index
     if Job.last.nil?
-      days_since_last_query = 8
-    else
-      last_api_query = Job.last.updated_at.to_datetime
-      days_since_last_query = (DateTime.now - last_api_query).to_i
+      api_query
+    end
+    # jobs
+  end
+
+  def jobs
+    @stats = {}
+
+    # @top = JobSkill.select("job_skills.skill_id, COUNT(skill_id) as skill_count").group("job_skills.skill_id").order("skill_count DESC").limit(10)
+    @top_skills = {
+      'Javascript' => ['Javascript', 'Javascript Frameworks', 'Javascript-JS Library'],
+      'Python / Django' => ['Python', 'Django', 'Django/Flask'],
+      'Ruby / Ruby on Rails' => ['Ruby on Rails', 'Ruby\Rails', 'Ruby', 'Rails', 'Backend Rails Development'],
+      'HTML & CSS' => ['CSS', 'HTML', 'HTML5 & CSS3', 'HTML5  CSS3', 'HTML/CSS', 'HTML5/CSS3'],
+      'iOS Development' => ['iOS Development', 'iOS', 'Objective C', 'Objective-C', 'Swift'],
+      'Java' => ['Java', 'Java Based Infrastructure And Frameworks', 'Java Based Infrastructure', 'Java Based Frameworks'],
+      'Node.js' => ['Node.js', 'Nodejs', 'NodeJs/Express', 'Node.js Developer', 'Node JS', 'Node'],
+      'Sales and Marketing' => ['Sales', 'Sales and Marketing'],
+      'PHP' => ['PHP', 'PHP5', 'PHP Frameworks', 'Laravel']
+    }
+    
+    # calculate @stats of top skills + PHP
+    @top_skills.each do |skill_name, skills_array|
+      skill_ids_array = Skill.where(:name => skills_array).pluck(:id)
+      add_stats(skill_name, skill_ids_array)
     end
 
-    api_query if (days_since_last_query > 7)
+    render json: @stats
+  end
 
-    @companies = Company.all
-    @jobs = Job.all
-    @skills = Skill.all
-    @jobroles = Jobrole.all
-    @jobskills = JobSkill.all
-    @jobjobrole = JobJobrole.all
+  def add_stats(skill_name, skills_array)
+    # find all jobs matching skill => returns array of job_ids
+    job_ids = JobSkill.where(:skill_id => skills_array).pluck('DISTINCT job_id')
+    # find compensation for jobs => returns array of compensation objects
+    comps = Compensation.where(:job_id => job_ids)
 
-    puts "first job"
-    puts Job.first
+    # loop through compensation data to set skill data
+    comps.each do |comp|
+      if !@stats.has_key?(skill_name) # add skill to @stats if does not yet exist
+        @stats[skill_name] = {
+          total_salary_min: comp.salary_max,
+          total_salary_max: comp.salary_max,
+          count: 1,
+        }
+      else # update @stats if it already exists
+        @stats[skill_name][:total_salary_min] += comp.salary_min
+        @stats[skill_name][:total_salary_max] += comp.salary_max
+        @stats[skill_name][:count] += 1
+      end
+    end
   end
 
   private
 
   def api_query
-    i = 1; max = false
+    i = 1; max = 2
     # API response is paginated so must call all pages individually
-    until i == 2 do
+    until i > max do
       data = HTTParty.get("https://api.angel.co/1/tags/1692/jobs?access_token=" +
         ENV["ANGELLIST_TOKEN"] + "&page=" + i.to_s)
       max = data["last_page"]
@@ -51,17 +85,20 @@ class MainController < ApplicationController
           company = Company.where(:angellist_id => job["startup"]["id"]).first
         end
           
-        if !Job.exists?(:angellist_id => job["id"])
-          jobtype = Jobtype.where(:name => job["job_type"]).first
+        # Save any new full-time SF job that does not yet exist in the database
+        if !Job.exists?(:angellist_id => job["id"]) && job["job_type"] == "full-time" &&
+            # discard salary outliers (based on analysis of all data)
+            !job["salary_min"].nil? && !job["salary_max"].nil? &&
+            job["salary_min"].between?(1, 300000) && job["salary_max"].between?(1, 500000)
+
           new_job = Job.create(
             :angellist_id => job["id"],
             :title => job["title"],
             :created => job["created_at"],
             :last_updated => job["updated_at"],
             :company => company,
-            :jobtype => jobtype
           )
-          # ...and add compensation
+          # ...and add related compensation
           Compensation.create(
             :job => new_job,
             :salary_min => job["salary_min"],
@@ -69,7 +106,7 @@ class MainController < ApplicationController
             :equity_min => job["equity_min"],
             :equity_max => job["equity_max"]
           )
-          # ...and add related skills/role to database
+          # add tags (skill or role) to database
           job["tags"].each do |tag|
             if tag["tag_type"] == "SkillTag"
               skill = Skill.where(
@@ -98,4 +135,5 @@ class MainController < ApplicationController
       i += 1
     end
   end
+
 end
