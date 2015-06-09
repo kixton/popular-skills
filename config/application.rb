af645bb25449b8581e5696bf6f4e06fdbd50add3
Kim Pham <ms.kimpham@gmail.com>
2015-06-08 17:03:34 -0700

    Configure app

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Pixypixy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.compile = true
+    config.assets.compress = true
+    config.assets.digest = true
+    config.assets.enabled = true
   end
 end
